c0137efd14d7a89c0671ff3ca903a52b34772f7b
Pauline Muigai <wanjirumuigai@gmail.com>
2023-03-12 12:44:41 +0300

    completed lab

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+d = ["doc", "dopey", "bashful", "cheddar"]
+def roll_call_dwarves(dwarfs)# code an argument here
+  a = dwarfs.to_enum 
+  a.with_index {|value, index| puts "#{index + 1}. #{value}"}
 end
+roll_call_dwarves(d)
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)# code an argument here
+  arr = planeteers.map do |item|
+    "#{item.capitalize}!"
+  end
+  arr
 end
+summon_captain_planet(d)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+ arr.each do |word|
+    if word.length > 4
+      return true
+       end
+        end
+    false
+  
 end
+long_planeteer_calls(d)
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+arr.find do |item|
+ if cheese_types.include?(item) == true
+  
+    item
+ end
 end
+    
+end
+find_the_cheese(d)
